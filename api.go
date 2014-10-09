4d2d822f3d7c65b19a1907e69a9f5bb392bf02ec
Paul Querna <pquerna@apache.org>
2014-10-09 15:47:51 -0700

    start sketching out the api

--- api.go
+++ api.go
@@ -23,14 +23,74 @@ import (
 
 type HashFunc func(v string)
 
-type Hasher interface {
-	Hasher(r request.Request, hf HashFunc)
+type Hash interface {
+	// opaque hashing function, call for each string you
+	// want included to make a request unique.
+	Hash(r request.Request, hf HashFunc)
+}
+
+type RecvCmd int
+
+const (
+	RECV_LOOKUP RecvCmd = iota
+	RECV_PASS   RecvCmd = iota
+	RECV_REJECT RecvCmd = iota
+)
+
+type Recv interface {
+	Recv(r request.Request) RecvCmd
+}
+
+type CacheGetCmd int
+
+const (
+	CACHE_GET_HIT  CacheGetCmd = iota
+	CACHE_GET_MISS CacheGetCmd = iota
+	CACHE_GET_PASS CacheGetCmd = iota
+)
+
+type CacheStoreCmd int
+
+const (
+	CACHE_STORE_STORED CacheStoreCmd = iota
+	CACHE_STORE_MISS   CacheStoreCmd = iota
+)
+
+type CachedResponse interface {
+	// TODO: more.
+}
+
+type Cache interface {
+	Get(key string, r request.Request) (CacheGetCmd, CachedResponse)
+	Store(key string, r request.Request, a request.Attempt) CacheStoreCmd
+}
+
+type MissCmd int
+
+const (
+	MISS_FETCH MissCmd = iota
+	MISS_PASS  MissCmd = iota
+)
+
+type Miss interface {
+	Miss(r request.Request) MissCmd
 }
 
 type HttpCacher struct {
-	hasher Hasher
+	recv  Recv
+	hash  Hash
+	cache Cache
+	miss  Miss
 }
 
 func NewHttpCacher() *HttpCacher {
-	return &HttpCacher{}
+	bc := newBasicCache()
+	cache := &HttpCacher{
+		recv:  bc,
+		hash:  bc,
+		cache: bc,
+		miss:  bc,
+	}
+	// figure out how to best do this: overrides interface{}
+	return cache
 }
---
+++ cache.go
@@ -0,0 +1,48 @@
+/**
+ *  Copyright 2014 Paul Querna
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package vulcancache
+
+import (
+	"github.com/mailgun/vulcan/request"
+)
+
+type basicCache struct{}
+
+func newBasicCache() *basicCache {
+	return &basicCache{}
+}
+
+func (bc *basicCache) Recv(r request.Request) RecvCmd {
+	return RECV_LOOKUP
+}
+
+func (bc *basicCache) Hash(r request.Request, hf HashFunc) {
+
+}
+
+func (bc *basicCache) Get(key string, r request.Request) (CacheGetCmd, CachedResponse) {
+	return CACHE_GET_MISS, nil
+}
+
+func (bc *basicCache) Store(key string, r request.Request, a request.Attempt) CacheStoreCmd {
+	return CACHE_STORE_MISS
+}
+
+func (bc *basicCache) Miss(r request.Request) MissCmd {
+	return MISS_FETCH
+}
